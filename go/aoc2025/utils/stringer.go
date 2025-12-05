5c0d88de94f8dc814fd22bd5454d8980068949ae
Scott Templeton <scott.templeton@vistair.com>
2025-12-05 10:30:34 +0000

    day5

--- go/aoc2025/utils/stringer.go
+++ go/aoc2025/utils/stringer.go
@@ -2,7 +2,6 @@ package utils
 
 import "slices"
 
-// ReverseString reverses a string, handling Unicode correctly
 func ReverseString(input string) string {
 	runes := []rune(input)
 	slices.Reverse(runes)
